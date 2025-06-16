b943cee90703afbfc79efacb7cde96557df6cdd8
Yong-Cheng <frank0929649689@gmail.com>
2025-06-16 23:49:56 +0800

    first commit

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "../css/Favorites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
+
+function Favorites() {
+  const { favorites } = useMovieContext();
+
+  if (favorites.length > 0) {
+    return (
+      <div className="favorites">
+        <h2>Your Favorite Movies</h2>
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="favorites-empty">
+        <h2>No favorites yet</h2>
+        <p>Add your favorite movies to your favorites list</p>
+      </div>
+    );
+  }
+}
+
+export default Favorites;
